47ea2db1579552a0d2874d5d4c484a35bc250c2c
Jonas <contact@atalantus.de>
2020-02-01 13:37:20 +0100

    removed debug

--- src/app/rete/rete.component.ts
+++ src/app/rete/rete.component.ts
@@ -24,10 +24,6 @@ export class ReteComponent implements AfterViewInit {
 
     const editor = new NodeEditor('demo@0.2.0', container);
     editor.use(ConnectionPlugin);
-
-    console.log(AngularRenderPlugin);
-    console.log(require('rete-angular-render-plugin'));
-
     editor.use(AngularRenderPlugin);
 
     const engine = new Engine('demo@0.2.0');
